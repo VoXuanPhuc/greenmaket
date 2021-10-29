690974a9fe642b121165c4daed5e0666652503a2
VoXuanPhuc <phucpro746@gmail.com>
2021-10-29 22:09:25 +0700

    update Main

--- app/src/main/java/com/app/laptrinhdidong/DanhMucActivity.java
+++ app/src/main/java/com/app/laptrinhdidong/DanhMucActivity.java
@@ -1,8 +1,13 @@
 package com.app.laptrinhdidong;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class DanhMucActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class DanhMucActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_danh_muc);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavi);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home :
+                        break;
+                    case R.id.card :
+                        break;
+                    case R.id.notification:
+                        break;
+                    case R.id.profile:
+                        startActivity(new Intent(DanhMucActivity.this, Activity_dangnhap.class));
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
--- app/src/main/java/com/app/laptrinhdidong/MainActivity.java
+++ app/src/main/java/com/app/laptrinhdidong/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavi);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
