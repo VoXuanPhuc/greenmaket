89874c0a8d9cf2f3a75b7a18c503d67aa6ef5365
trandai2401 <1911505310207@sv.ute.udn.vn>
2021-10-30 04:57:50 +0700

    Hoan thanh

---
+++ app/src/main/java/com/app/laptrinhdidong/SanhamTheoDanhMucActivity.java
@@ -0,0 +1,14 @@
+package com.app.laptrinhdidong;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SanhamTheoDanhMucActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sanham_theo_danh_muc);
+    }
+}
