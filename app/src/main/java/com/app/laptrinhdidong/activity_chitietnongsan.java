52b5faf930a5b1d29a1c67bdf047d1b28de7d90f
VoXuanPhuc <phucpro746@gmail.com>
2021-11-03 21:23:44 +0700

    Phuc them su kien chitietsanpham

--- app/src/main/java/com/app/laptrinhdidong/activity_chitietnongsan.java
+++ app/src/main/java/com/app/laptrinhdidong/activity_chitietnongsan.java
@@ -3,12 +3,55 @@ package com.app.laptrinhdidong;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class activity_chitietnongsan extends AppCompatActivity {
 
+    ImageButton add;
+    ImageButton minus;
+    ImageButton love;
+    TextView number;
+    Integer sl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chitietnongsan);
+
+        add = (ImageButton) findViewById(R.id.them);
+        minus = (ImageButton) findViewById(R.id.giam);
+        love = (ImageButton) findViewById(R.id.yeuthichbaidang);
+        number = (TextView) findViewById(R.id.soluongctns);
+
+        sl = Integer.parseInt(number.toString());
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sl += 1;
+                number.setText(sl.toString());
+            }
+        });
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (sl > 1) {
+                    sl += 1;
+                    number.setText(sl.toString());
+                } else {
+                    Toast.makeText(activity_chitietnongsan.this, "Số lượng tối thiểu là 1", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+        love.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                love.setImageResource(R.drawable.tym_icon);
+            }
+        });
+
     }
 }
