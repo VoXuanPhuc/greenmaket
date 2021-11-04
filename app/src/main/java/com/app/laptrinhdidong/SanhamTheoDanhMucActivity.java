3a62752e17e6ae529053d0a4c3ab0971aa66727f
trandai2401 <1911505310207@sv.ute.udn.vn>
2021-11-04 22:40:56 +0700

    Tạo sự kiện mở Chi tiết nông sản - Đại

--- app/src/main/java/com/app/laptrinhdidong/SanhamTheoDanhMucActivity.java
+++ app/src/main/java/com/app/laptrinhdidong/SanhamTheoDanhMucActivity.java
@@ -1,14 +1,34 @@
 package com.app.laptrinhdidong;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SanhamTheoDanhMucActivity extends AppCompatActivity {
-
+    ConstraintLayout theSanPham1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sanham_theo_danh_muc);
+
+        theSanPham1 = findViewById(R.id.ctheSanPham1);
+//        theSanPham1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(SanhamTheoDanhMucActivity.this, activity_chitietnongsan.class));
+//
+//            }
+//        });
+
+
+    }
+
+
+    public void moChiTietNongSan(View view) {
+                        startActivity(new Intent(SanhamTheoDanhMucActivity.this, activity_chitietnongsan.class));
+
     }
 }
