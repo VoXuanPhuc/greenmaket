e9bbbc92c39fafa12b86d165a6ea8e17031ee83c
VoXuanPhuc <phucpro746@gmail.com>
2021-11-05 13:28:34 +0700

    Thuy update icon search menu

--- app/src/main/java/com/app/laptrinhdidong/activity_profile.java
+++ app/src/main/java/com/app/laptrinhdidong/activity_profile.java
@@ -14,16 +14,12 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class activity_profile extends AppCompatActivity {
 
     Button editProfile;
-    Button managerBaiDang;
     Button hoaDon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
-
         editProfile = (Button) findViewById(R.id.editProfile);
-        managerBaiDang = (Button) findViewById(R.id.quanlybd);
         hoaDon = (Button) findViewById(R.id.hoadon);
         editProfile.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,17 +27,11 @@ public class activity_profile extends AppCompatActivity {
                 startActivity(new Intent(activity_profile.this, com.app.laptrinhdidong.editProfile.class));
             }
         });
-        managerBaiDang.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(activity_profile.this, com.app.laptrinhdidong.activity_quanlynongsan.class));
-            }
-        });
 
         hoaDon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(activity_profile.this,com.app.laptrinhdidong.DanhSachHoaDonActivity.class));
+                startActivity(new Intent(activity_profile.this, DanhSachHoaDonActivity.class));
             }
         });
 
