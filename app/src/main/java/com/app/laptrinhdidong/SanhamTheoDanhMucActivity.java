71d4c613cdff76af3cec30252fea15f827ad0f92
VoXuanPhuc <phucpro746@gmail.com>
2021-11-05 10:05:27 +0700

    Phuc cap nhat su kien cho danh muc

--- app/src/main/java/com/app/laptrinhdidong/SanhamTheoDanhMucActivity.java
+++ app/src/main/java/com/app/laptrinhdidong/SanhamTheoDanhMucActivity.java
@@ -2,26 +2,22 @@ package com.app.laptrinhdidong;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
 public class SanhamTheoDanhMucActivity extends AppCompatActivity {
 
-    ConstraintLayout constraintLayout1;
+    ConstraintLayout theSanPham1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sanham_theo_danh_muc);
+        theSanPham1 = findViewById(R.id.ctheSanPham1);
 
-        constraintLayout1 = (ConstraintLayout) findViewById(R.id.sanpham1);
 
-        constraintLayout1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(SanhamTheoDanhMucActivity.this, activity_chitietnongsan.class));
-            }
-        });
+    }
+    public void moChiTietNongSan(View view) {
+        startActivity(new Intent(SanhamTheoDanhMucActivity.this, activity_chitietnongsan.class));
+
     }
 }
