591a9294e049987dace751227fd4c7b564866d9f
hovananh-11 <1911505310102@sv.ute.udn.vn>
2021-12-09 00:05:17 +0700

    Thêm nut skip, show hoạt động một lần khi chạy lần đầu cho giao diện

--- app/src/main/java/com/app/laptrinhdidong/giaodien4.java
+++ app/src/main/java/com/app/laptrinhdidong/giaodien4.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class giaodien4 extends AppCompatActivity {
@@ -14,9 +15,18 @@ public class giaodien4 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_giaodien4);
         ImageButton nextbtn;
+        Button bnskip;
         nextbtn = (ImageButton) findViewById(R.id.next);
+        bnskip = (Button) findViewById(R.id.btnskip) ;
 
         nextbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(giaodien4.this, DanhMucActivity.class));
+            }
+        });
+
+        bnskip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(giaodien4.this, DanhMucActivity.class));
